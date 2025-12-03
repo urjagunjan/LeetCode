bdc7d0107c8e987c7ebd31daae1c15fe390e55da
urja gunjan <urjagunjansrivastava@gmail.com>
2025-12-03 10:57:48 +0530

    Time: 1 ms (10.57%), Space: 41.9 MB (47.82%) - LeetHub

---
+++ 0062-unique-paths/0062-unique-paths.java
@@ -0,0 +1,20 @@
+class Solution {
+    public int uniquePaths(int m, int n) {
+        Integer[][] dp=new Integer[m][n];
+        for(int i=m-1;i>=0;i--){
+            for(int j=n-1;j>=0;j--){
+                if(i==m-1 || j==n-1){
+                    dp[i][j]=1;
+                }
+                else{
+                    int right=dp[i][j+1];
+                    int down=dp[i+1][j];
+                    dp[i][j]=right+down;
+                }
+            }
+        }
+        return dp[0][0];
+        
+
+    }     
+}
