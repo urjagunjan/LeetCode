e032a00d8b85d9dd72d4af28dc0063d215acc13f
urja gunjan <urjagunjansrivastava@gmail.com>
2026-04-23 23:45:37 +0530

    Time: 0 ms (100%), Space: 42.2 MB (38.15%) - LeetHub

--- 0070-climbing-stairs/0070-climbing-stairs.java
+++ 0070-climbing-stairs/0070-climbing-stairs.java
@@ -1,22 +1,11 @@
 class Solution {
     public int climbStairs(int n) {
         int[] dp=new int[n+1];
-        Arrays.fill(dp,-1);
-        return helper(n,dp);
-    }
-    public int helper(int n,int[] dp){
-        if(n==0){
-            return 1;
+        dp[0]=1;
+        dp[1]=1;
+        for(int i=2;i<=n;i++){
+            dp[i]=dp[i-1]+dp[i-2];
         }
-        if(n==1){
-            return 1;
-        }
-        if(n<0){
-            return 0;
-        }
-        if(dp[n]!=-1){
-            return dp[n];
-        }
-        return dp[n]=helper(n-1,dp)+helper(n-2,dp);
+        return dp[n];
     }
 }
