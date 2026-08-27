ebd7eace111125fbab1893e51c1f0481f2e1f187
urja gunjan <urjagunjansrivastava@gmail.com>
2026-08-28 01:43:52 +0530

    Time: 17 ms (70.52%), Space: 64 MB (7.14%) - LeetHub

---
+++ 0044-wildcard-matching/0044-wildcard-matching.java
@@ -0,0 +1,36 @@
+class Solution {
+    Boolean[][] dp;
+    public boolean isMatch(String s, String p) {
+        int i=s.length()-1;
+        int j=p.length()-1;
+        dp=new Boolean[i+1][j+1];
+        return solver(s,p,i,j);
+    }
+    private boolean solver(String s,String p,int i,int j){
+        
+        if(i<0 && j<0)return true;
+
+        if(j<0)return false;
+        if(i<0){
+            for(int k=0;k<=j;k++){
+                if(p.charAt(k)!='*')return false;
+            }
+            return true;
+        }
+
+        if(s.charAt(i)==p.charAt(j)|| p.charAt(j)=='?'){
+            return solver(s,p,i-1,j-1);
+        }
+        if(dp[i][j]!=null)return dp[i][j];
+
+        if(p.charAt(j)=='*'){
+
+            boolean skipstar=solver(s,p,i-1,j);
+            boolean notskipstar=solver(s,p,i,j-1);
+
+            return dp[i][j]=skipstar||notskipstar;
+        }
+
+        return false;
+    }
+}
