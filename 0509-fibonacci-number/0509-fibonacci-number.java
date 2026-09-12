f994081afca991c7a3c554632f2bc89e651b1220
urja gunjan <urjagunjansrivastava@gmail.com>
2026-09-13 04:05:36 +0530

    Time: 0 ms (100%), Space: 42.3 MB (16.09%) - LeetHub

--- 0509-fibonacci-number/0509-fibonacci-number.java
+++ 0509-fibonacci-number/0509-fibonacci-number.java
@@ -5,13 +5,8 @@ class Solution {
         dp[0]=0;
         dp[1]=1;
         for(int i=2;i<=n;i++){
-            dp[i]=dp[i-1]+dp[i-2];
+            dp[i]=dp[i-2]+dp[i-1];
         }
         return dp[n];
     }
-    // private int helper(int n){
-    //     if(n<2)return n;
-    //     if(dp[n]!=null)return dp[n];
-    //     return dp[n]=fib(n-1)+fib(n-2);
-    // }
 }
